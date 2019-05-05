933e02bc98fb1fd116bd00e0fa27eef844e350fc
Martin Matovu <matovu.synergy@gmail.com>
2019-05-05 14:04:11 +0300

    Added my solutions for min(), isEven(), countBs() and countChar()

---
+++ 2_Functions.js
@@ -0,0 +1,76 @@
+// Functions - Exercise
+
+/**
+ * Minimum
+ * 
+ * Just like the standard function Math.min that returns its smallest argument,
+ * Write a function min() that takes two arguments and returns their minimum. 
+ */
+function min(a, b){
+    if(a<b) return a;
+    if(a>b) return b;
+}
+
+
+/**
+ * Recursion
+ * 
+ * We know that % (remainder operator) can test if a number is even or odd 
+ * by using % 2 to see whether it’s divisible by two. 
+ * Here’s another way to define if a positive whole number is even or odd:
+ * • Zero is even, One is odd and any other number N, its evenness is the same as N - 2.
+ * 
+ * Define a recursive function isEven() corresponding to this description. 
+ * The function should accept a single parameter (a positive, whole number) and return a Boolean.
+ * Test it on 50 and 75. See how it behaves on -1. (Why? Can you think of a way to fix this?) 
+ */
+function isEven(n) {
+    if(n==0) {
+        return "True";
+    } else if(n==1) {
+        return "False";
+    } else {
+        while(n>1){
+            n -= 2;
+            return isEven(n);
+        }
+    }
+}
+
+
+/**
+ * Bean counting
+ * 
+ * You can get the Nth character, or letter, from a string by writing "string"[N].
+ * The returned value will be a string containing only one character (for example, "b"). 
+ * The first character has position 0, which causes the last one to be found at position string.length - 1. 
+ * (In other words, a two-character string has length 2, and its characters have positions 0 and 1.)
+ * 
+ * Write a function countBs() that takes a string as its only argument and 
+ * returns a number that indicates how many uppercase “B” characters there are in the string.
+ */
+function countBs(str) {
+    let count = 0;
+    for(let x=0; x<str.length; x++ ) {
+        if(str[x] == "B"){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+/**
+ * Next, write a function called countChar() that behaves like countBs(), 
+ * except it takes a second argument that indicates the character that is to be counted
+ * (rather than counting only uppercase “B” characters). 
+ * Rewrite countBs() to make use of this new function.
+ */
+function countChar(word, letter) {
+    let count = 0;
+    for(let x=0; x<word.length; x++ ) {
+        if(str[x] == letter){
+            count += 1;
+        }
+    }
+    return count;
+}
