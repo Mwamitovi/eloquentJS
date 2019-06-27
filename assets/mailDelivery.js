05d6c8d35f25810a8652358d706feaad0bca9c4d
Martin Matovu <matovu.synergy@gmail.com>
2019-06-27 14:14:08 +0300

    Updated "runRobot" to "mailDelivery", and added output from routeRobot also...

--- assets/mailDelivery.js
+++ assets/mailDelivery.js
@@ -1,6 +1,7 @@
 
 // run on June/27/2019
 
+// using random strategy
 runRobot(VillageState.random(), randomRobot);
 
 // VM115:10 Moved to Mengo Hospital
@@ -60,3 +61,27 @@ runRobot(VillageState.random(), randomRobot);
 // VM115:10 Moved to Mengo Hospital
 // VM115:10 Moved to Namirembe Cathedral
 // VM115:4 Done in 56 turns
+
+
+// using a mailRoute
+runRobot(VillageState.random(), routeRobot)
+
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Lungujja Progressive Primary
+// VM115:10 Moved to Lubiri Sec. School
+// VM115:10 Moved to Victory City Church
+// VM115:10 Moved to Lubiri Sec. School
+// VM115:10 Moved to Rubaga Hospital
+// VM115:10 Moved to Miracle Center
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Lungujja Progressive Primary
+// VM115:4 Done in 18 turns
