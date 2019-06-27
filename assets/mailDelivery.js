87b859fe140a3b62d98230f8ec331b54aa16a79e
Martin Matovu <matovu.synergy@gmail.com>
2019-06-27 15:12:29 +0300

    Add test-case-3 results from Chrome

--- assets/mailDelivery.js
+++ assets/mailDelivery.js
@@ -85,3 +85,25 @@ runRobot(VillageState.random(), routeRobot)
 // VM115:10 Moved to Mengo Hospital
 // VM115:10 Moved to Lungujja Progressive Primary
 // VM115:4 Done in 18 turns
+
+
+// test-case-3: using a work-plan
+runRobot(VillageState.random(), goalOrientedRobotoo)
+
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Lungujja Progressive Primary
+// VM115:10 Moved to Lubiri Sec. School
+// VM115:10 Moved to Rubaga Hospital
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Rubaga Hospital
+// VM115:10 Moved to Lubiri Sec. School
+// VM115:4 Done in 16 turns
