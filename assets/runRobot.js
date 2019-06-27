4d8ab2ad2331fa80c25330b4a2d093ec32c6f115
Martin Matovu <matovu.synergy@gmail.com>
2019-06-27 13:33:54 +0300

    A full script result, showing runRobot() random movement turns

---
+++ assets/runRobot.js
@@ -0,0 +1,62 @@
+
+// run on June/27/2019
+
+runRobot(VillageState.random(), randomRobot);
+
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Lungujja Progressive Primary
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Rubaga Hospital
+// VM115:10 Moved to Lubiri Sec. School
+// VM115:10 Moved to Rubaga Hospital
+// VM115:10 Moved to Victory City Church
+// VM115:10 Moved to Rubaga Hospital
+// VM115:10 Moved to Miracle Center
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Old Kampala Mosque
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Miracle Center
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to New Taxi Park
+// VM115:10 Moved to Shell Rubaga
+// VM115:10 Moved to Mengo Post Office
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Lungujja Progressive Primary
+// VM115:10 Moved to Mengo Hospital
+// VM115:10 Moved to Namirembe Cathedral
+// VM115:4 Done in 56 turns
