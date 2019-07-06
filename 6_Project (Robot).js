60de73f63c17a67d0e7c847473590ff574d4f6ee
Martin Matovu <matovu.synergy@gmail.com>
2019-07-06 11:36:51 +0300

    Exercise: define compareRobots() that compares robot efficiency. Calls countSteps() function to calculate steps needed by each robot to execute the same tasks.

--- 6_Project (Robot).js
+++ 6_Project (Robot).js
@@ -436,3 +436,56 @@ runRobot(VillageState.random(), goalOrientedRobot);
 // → Moved to Namirembe Cathedral
 // → ...
 // → Done in 16 turns
+
+
+// -------------------- Robot - Exercise ---------------------------
+
+
+/**
+ * Measuring a robot
+ * 
+ * It’s hard to objectively compare robots by just letting them solve a few scenarios.
+ * Write a function compareRobots() that takes two robots (and their starting memory).
+ * It should generate 100 tasks and let each of the robots solve each of these tasks. 
+ * When done, it should output the average number of steps each robot took per task.
+ * 
+ * For the sake of fairness, make sure you give each task to both robots, 
+ * rather than generating different tasks per robot.
+ */
+// 
+function countSteps(state, robot, memory) {
+   for(let steps=0;; steps++) {
+      if(state.ourMail.length == 0) return steps;
+      let action = robot(state, memory);
+      state = state.move(action.direction);
+      memory = action.memory;
+   }
+}
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+   let total1 = 0, total2 = 0;
+   for(let i=0; i<100; i++) {
+      let state = VillageState.random();
+      total1 += countSteps(state, robot1, memory1);
+      total2 += countSteps(state, robot2, memory2);
+   }
+   console.log(`Robot 1 needed ${total1 / 100} steps per task`)
+   console.log(`Robot 2 needed ${total1 / 100}`)
+}
+
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
+// → Robot 1 needed 19.51 steps per task
+// → Robot 2 needed 19.51
+
+
+/**
+ * Robot efficiency
+ * 
+ * Can you write a robot that finishes the delivery task faster than goalOrientedRobot? 
+ * If you observe that robot’s behavior, what obviously stupid things does it do? 
+ * How could those be improved?
+ * 
+ * If you solved the previous exercise, 
+ * you might want to use your compareRobots function to verify whether you improved the robot. 
+ */
