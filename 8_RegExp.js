68252bd6fed9297d23306b38224f55bfad69efca
Martin Matovu <matovu.synergy@gmail.com>
2019-07-29 09:33:27 +0300

    Create "RegExp" file, for the regular expressions exercise

---
+++ 8_RegExp.js
@@ -0,0 +1,23 @@
+// Regular Expressions - Exercise
+
+// https://debuggex.com - useful online tool for debugging regex
+
+/**
+ * RegExp Golf
+ * 
+ * "Code golf" is a term used for 
+ * the game of trying to express a particular program in as few characters as possible. 
+ * Similarly, "regexp golf" is the 
+ * practice of writing as tiny a regular expression as possible to 
+ * match a given pattern, and only that pattern.
+ * 
+ * For each of the following items, 
+ * write a regular expression to test whether any of the given substrings occur in
+ * a string. The regular expression should match only strings containing one of the
+ * substrings described. Do not worry about word boundaries unless explicitly mentioned. 
+ * When your expression works, see whether you can make it any smaller.
+ * 
+ * Test each solution with a few test strings.
+ */
+
+// car and cat
